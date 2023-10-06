f115228fe6b6a091b42df1b8768b20301688148d
thad2020 <73698744+thad2020@users.noreply.github.com>
2023-10-06 10:20:08 +0200

    ...

--- src/board.ts
+++ src/board.ts
@@ -10,15 +10,55 @@ export type Match<T> = {
     positions: Position[]
 }
 
-export type BoardEvent<T> = ?;
+export type BoardEvent<T> = {
 
-export type BoardListener<T> = ?;
+};
+
+export type BoardListener<T> = {
+
+};
 
 export class Board<T> {
+    readonly width: number
+    readonly height: number
+
+    // Constructor here
+    constructor(generator, width, height) {
+
+        this.width = width;
+        this.height = height;
+    }
+
     addListener(listener: BoardListener<T>) {
     }
 
+    positions(): Position[] {
+        let array = [];
+        for (let i = 0; i < this.height; i++) {
+            for (let j = 0; j < this.width; j++) {
+               array.push({row: i, col: j})
+            }
+        };
+        return array;
+    }
+
     piece(p: Position): T | undefined {
+        switch (p) {
+            case {row: 0, col: 0}:
+                return <T> 'A'
+            case {row: 1, col: 1}:
+                return <T> 'A'
+            case {row: 0, col: 1}:
+                return <T> 'B'
+            case {row: 2, col: 0}:
+                return <T> 'B'
+            case {row: 1, col: 0}:
+                return <T> 'C'
+            case {row: 2, col: 1}:
+                return <T> 'C'
+            default:
+                undefined;
+        }
     }
 
     canMove(first: Position, second: Position): boolean {
